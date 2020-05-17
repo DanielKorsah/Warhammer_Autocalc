5b5575fcc30767d8b3bc7246d7c5f9eed8d3fd6f
Daniel Korsah <dkorsah15@aol.co.uk>
2020-05-17 23:48:15 +0100

    Display results on screen.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI HitRollText;
+    public TextMeshProUGUI HitResultText;
+    public TextMeshProUGUI WoundRollText;
+    public TextMeshProUGUI WoundThresholdText;
+    public TextMeshProUGUI WoundResultText;
+    void Start()
+    {
+        Calculator.HitValue.AddListener(SetHitRoll);
+        Calculator.HitResult.AddListener(SetHitResult);
+        Calculator.WoundValue.AddListener(SetWoundRoll);
+        Calculator.WoundThreshold.AddListener(SetWoundThreshold);
+        Calculator.WoundResult.AddListener(SetWoundResult);
+    }
+
+    void SetHitRoll(int rollValue)
+    {
+        HitResultText.text = rollValue.ToString();
+    }
+
+    void SetHitResult(bool result)
+    {
+        if (result)
+        {
+            HitRollText.text = $"<color=red>Hit!</color>";
+        }
+        else
+        {
+            HitRollText.text = $"<color=yellow>Miss!</color>";
+        }
+    }
+
+    void SetWoundRoll(int rollValue)
+    {
+        if (rollValue == 0)
+        {
+            WoundRollText.text = "~";
+        }
+        else
+        {
+            WoundRollText.text = rollValue.ToString();
+        }
+
+    }
+
+    void SetWoundThreshold(int threshold)
+    {
+        if (threshold == 0)
+        {
+            WoundThresholdText.text = "~";
+        }
+        else
+        {
+            WoundThresholdText.text = threshold.ToString();
+        }
+    }
+
+    void SetWoundResult(bool wounded)
+    {
+        if (wounded)
+        {
+            WoundResultText.text = $"<color=red>Wounded!</color>";
+        }
+        else
+        {
+            WoundResultText.text = $"<color=yellow>Not Wounded!</color>";
+        }
+    }
+}
